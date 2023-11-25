b6136086cd01810cac8004c338ae6a2fbddd5891
まえはらひゅうが <>
2023-11-25 16:47:36 +0900

    隠しファイル作った

---
+++ fueoni_ver2/ios/Runner/Environment.swift
@@ -0,0 +1,12 @@
+//
+//  Environment.swift
+//  Runner
+//
+//  Created by まえはらひゅうが on 2023/11/25.
+//
+
+import Foundation
+
+struct Env{
+    static let googleMapApiKey = "<AIzaSyBhmrzxTqC2Np5z4AD5a2pdGp48bSY7xWg>"
+}
