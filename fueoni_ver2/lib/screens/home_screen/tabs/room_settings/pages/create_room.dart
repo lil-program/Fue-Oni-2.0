c9c5e2fc41bc7fa05635fbacf5ea9f32e5ac127d
{uchida} <{uchidadaiki1124@gmail.com}>
2023-12-03 11:42:29 +0900

    initMapScreenの変更

--- fueoni_ver2/lib/screens/home_screen/tabs/room_settings/pages/create_room.dart
+++ fueoni_ver2/lib/screens/home_screen/tabs/room_settings/pages/create_room.dart
@@ -27,22 +27,4 @@ class CreateRoomPageState extends State<CreateRoomPage> {
       ),
     );
   }
-
-/*
-  Future<void> initMapScreen() async {
-    // 位置情報の許可をリクエストする
-    LocationPermission permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      await Geolocator.requestPermission();
-    }
-
-    // FirebaseAuthの現在のユーザーを取得（必要に応じて）
-    final user = FirebaseAuth.instance.currentUser;
-    if (user == null) {
-      // ユーザーがログインしていない場合の処理（必要に応じて）
-    }
-
-    // 他に必要な初期化処理があればここに記述
-  }
-  */
 }
